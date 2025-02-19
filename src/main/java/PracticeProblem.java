091e20210e2df67eaf049373af31ee9710aabb65
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-02-19 16:54:45 +0000

    I COMMIT

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,32 @@ public class PracticeProblem {
 
 	}
 
-	//Write your functions here
+	public static String evenOrOdd(int num){
+		if (num % 2 == 0){
+			return "Even";
+		}else{
+			return "Odd";
+		}
+	}
+
+	public static String teacherOrStudent(String teacher){
+		if (teacher.equals("Kalisz")){
+			return "Teacher";
+		}else{
+			return "Student";
+		}
+	}
+
+	public static int fartherFromZero(int num){
+		if (num > 0 ){
+			num = num + 5;
+			return num;
+		}else if (num < 0){ 
+			num  = num - 5;
+			return num;	
+		}else{
+			return 0;
+		}
+	}
 
 }
